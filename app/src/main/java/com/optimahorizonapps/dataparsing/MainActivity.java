17d3645aea2eefd356af7f8b8cb5c9113c63e870
leonardo1313 <leonardo.novak5@gmail.com>
2021-09-06 11:39:43 +0200

    RequestQueue and StringRequest created and tested.

--- app/src/main/java/com/optimahorizonapps/dataparsing/MainActivity.java
+++ app/src/main/java/com/optimahorizonapps/dataparsing/MainActivity.java
@@ -3,12 +3,37 @@ package com.optimahorizonapps.dataparsing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue;
+    String url = "https://ww.google.com";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            // display the content of url
+            Log.d("Main", "onCreate: " + response.substring(0, 500));
+
+        }, error -> {
+            Log.d("Google", "Error!");
+        });
+
+        // add the request to RequestQueue
+        queue.add(stringRequest);
     }
 }
