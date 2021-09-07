0d5a02b26f61687bbb3da7e0f4dd30b593ec785b
leonardo1313 <leonardo.novak5@gmail.com>
2021-09-07 12:08:32 +0200

    JsonArrayRequest and JsonObjectRequest created and tested. Everything working!

--- app/src/main/java/com/optimahorizonapps/dataparsing/MainActivity.java
+++ app/src/main/java/com/optimahorizonapps/dataparsing/MainActivity.java
@@ -4,18 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     RequestQueue queue;
     String url = "https://ww.google.com";
+    String apiUrl = "https://jsonplaceholder.typicode.com/todos";
+    String apiObjectUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
 
     @Override
@@ -24,7 +32,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         queue = Volley.newRequestQueue(this);
+        TextView textView = findViewById(R.id.main_textView);
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, apiObjectUrl, null,
+                response -> {
+                    try {
+                        textView.setText(response.getString("title"));
+                        Log.d("JSONObject", "onCreate: " + response.getString("title"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, error -> {
+            Log.d("JSONObject", "onCreate: Failed!");
+                });
+
+        queue.add(jsonObjectRequest);
+
+//        getJsonArrayRequest();
+
+//        getString();
+    }
+
+    private void getJsonArrayRequest() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null,
+                response -> {
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            Log.d("JSON", "onCreateOK: " + jsonObject.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                },
+                error -> {
+            Log.d("JSON", "onCreate: Failed!");
+                });
+
+        queue.add(jsonArrayRequest);
+    }
+
+    private void getString() {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
             // display the content of url
             Log.d("Main", "onCreate: " + response.substring(0, 500));
